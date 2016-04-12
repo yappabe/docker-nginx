ff7c8d0833a7516d8c518c215c78ac94519f79f0
Joeri Verdeyen <info@jverdeyen.be>
2016-04-12 11:12:13 +0200

    Added ssl support on the 1.9 version

--- 1.9/run.sh
+++ 1.9/run.sh
@@ -2,4 +2,6 @@
 echo "upstream php-upstream { server $PHP_FPM_SOCKET; }" > /etc/nginx/conf.d/upstream.conf && \
 sed -i "s|DOCUMENT_ROOT|$DOCUMENT_ROOT|g" /etc/nginx/sites-enabled/default && \
 sed -i "s|INDEX_FILE|$INDEX_FILE|g" /etc/nginx/sites-enabled/default && \
+sed -i "s|DOCUMENT_ROOT|$DOCUMENT_ROOT|g" /etc/nginx/sites-enabled/default-ssl && \
+sed -i "s|INDEX_FILE|$INDEX_FILE|g" /etc/nginx/sites-enabled/default-ssl && \
 nginx
